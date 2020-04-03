ea7a032ba7f1503e327f0115ba83b807841b74f1
Michal Jarczewski <michal.jar.mj@gmail.com>
2020-04-03 22:53:17 +0200

    Add first GET tests with Rest Assured

---
+++ src/test/java/GET/JsonPlaceholderGET_Test.java
@@ -0,0 +1,76 @@
+package GET;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static io.restassured.RestAssured.given;
+
+public class JsonPlaceholderGET_Test {
+
+    @Test
+    public void jsonPlaceholderReadAllUsers() {
+
+        Response response = given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/users");
+
+        Assertions.assertEquals(200, response.statusCode());
+
+        JsonPath json = response.jsonPath();
+
+        List<String> names = json.getList("name");
+
+        Assertions.assertEquals(10, names.size());
+    }
+
+    @Test
+    public void jsonPlaceholderReadOneUser() {
+        Response response = given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/users/1");
+        
+        Assertions.assertEquals(200, response.statusCode());
+
+        JsonPath json = response.jsonPath();
+
+        Assertions.assertEquals("Leanne Graham", json.get("name"));
+        Assertions.assertEquals("Bret", json.get("username"));
+        Assertions.assertEquals("Sincere@april.biz", json.get("email"));
+        Assertions.assertEquals("Kulas Light", json.get("address.street"));
+    }
+
+    @Test
+    public void jsonPlaceholderReadOneUserWithPathVariable() {
+        Response response = given()
+                .pathParam("userId", 1)
+                .when()
+                .get("https://jsonplaceholder.typicode.com/users/{userId}");
+
+        Assertions.assertEquals(200, response.statusCode());
+
+        JsonPath json = response.jsonPath();
+
+        Assertions.assertEquals("Leanne Graham", json.get("name"));
+        Assertions.assertEquals("Bret", json.get("username"));
+        Assertions.assertEquals("Sincere@april.biz", json.get("email"));
+        Assertions.assertEquals("Kulas Light", json.get("address.street"));
+    }
+
+    @Test
+    public void jsonPlaceholderReadOneUserWithQueryParams() {
+        Response response = given()
+                .queryParam("username", "Bret")
+                .when()
+                .get("https://jsonplaceholder.typicode.com/users");
+
+        Assertions.assertEquals(200, response.statusCode());
+
+        JsonPath json = response.jsonPath();
+
+        Assertions.assertEquals("Leanne Graham", json.getList("name").get(0));
+        Assertions.assertEquals("Bret", json.getList("username").get(0));
+        Assertions.assertEquals("Sincere@april.biz", json.getList("email").get(0));
+    }
+}
